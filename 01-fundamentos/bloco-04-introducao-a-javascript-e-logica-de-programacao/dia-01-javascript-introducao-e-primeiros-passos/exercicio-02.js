d037ea8ae26c919edf61f189bf5e84d7b33dfa77
lucas-da-silva <lucassouza00012345@gmail.com>
2022-05-19 15:23:37 -0300

    Adiciona os conteúdos do bloco-04 dia-04

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-introducao-e-primeiros-passos/exercicio-02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-introducao-e-primeiros-passos/exercicio-02.js
@@ -1,39 +1,56 @@
 // Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
-//Adição (a + b)
 
 const a = 8;
 const b = 4;
 
-console.log(a+b);
+//Adição (a + b)
+function soma(elementoA, elementoB) {
+    return a + b;
+};
 
 // Subtração (a - b)
-
-console.log(a-b);
+function subtracao(elementoA, elementoB) {
+    return a - b;
+};
 
 // Multiplicação (a * b)
-
-console.log(a*b);
+function multiplicacao (elementoA, elementoB) {
+    return a * b;
+};
 
 // Divisão (a / b)
-
-console.log(a/b);
+function divisao (elementoA, elementoB) {
+    return a / b;
+};
 
 // Módulo (a % b)
+function modulo (elementoA, elementoB) {
+    return a % b;
+};
 
-console.log(a%b);
+console.log(soma());
+console.log(subtracao());
+console.log(multiplicacao());
+console.log(divisao());
+console.log(modulo());
 
 // Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
-const n1 = 5;
+const n1 = 7;
 const n2 = 6;
 
-if (n1 > n2) {
-    console.log(n1);
-} else if (n1 == n2) {
-    console.log("Os dois números possuem o mesmo valor");
-} else {
-    console.log(n2);
-}
+function maior (elementoA, elementoB) {
+
+    if (n1 > n2) { 
+        return n1 + " é maior que o " + n2;
+    } else if (n1 == n2) {
+        return 'Os dois números são iguais'
+    } else {
+        return n2 + " é maior que " + n1;
+    };
+};
+
+console.log(maior())
 
 //Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
@@ -41,39 +58,50 @@ const numero1 = 4;
 const numero2 = 1;
 const numero3 = 8;
 
-if (numero1 > numero2 && numero1 > numero3) {
-    console.log(numero1);
-} else if (numero2 > numero1 && numero2 > numero3) {
-    console.log(numero2);
-} else if (numero3 > numero1 && numero3 > numero2) {
-    console.log(numero3);
-}
+function maior (elemento1, elemento2, elemento3) {
+    if (numero1 > numero2 && numero1 > numero3) {
+        return numero1;
+    } else if (numero2 > numero1 && numero2 > numero3) {
+        return numero2;
+    } else if (numero3 > numero1 && numero3 > numero2) {
+        return numero3;
+    };
+};
+
+console.log(maior())
 
 // Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
-const valor = -1;
+function positiveNegative (param1) {
+    if (valor > 0) {
+        return "positive";
+    } else if (valor === 0) {
+        return "zero";
+    } else {
+        return "negative";
+    };
+};
 
-if (valor > 0) {
-    console.log("positive");
-} else if (valor === 0) {
-    console.log("zero");
-} else {
-    console.log("negative");
-}
+const valor = -1;
+console.log(positiveNegative(valor));
 
 // Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+function triangulo (param1, param2, param3) {
+    if (param1 + param2 + param3 === 180) {
+        return true;
+    } else if (param1 < 0 || param2 < 0 || param3 < 0) {
+        return "ERRO";
+    } else {
+        return false;
+    };    
+};
 
 const anguloInterno1 = 50;
 const anguloInterno2 = 90;
 const anguloInterno3 = 30;
 
-if (anguloInterno1 + anguloInterno2 + anguloInterno3 === 180) {
-    console.log(true);
-} else if (anguloInterno1 < 0 || anguloInterno2 < 0 || anguloInterno3 < 0) {
-    console.log("ERRO");
-} else {
-    console.log(false);
-}
+console.log(triangulo(anguloInterno1, anguloInterno2, anguloInterno3));
 
 // Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-parte-02-funcoes.js
@@ -0,0 +1,98 @@
+// 1- Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
+function verificaPalindrome (string) {
+    let inverso = string.split("").reverse().join("");
+    if (string === inverso) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+console.log(verificaPalindrome("arara"));
+
+// 2- Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Array de teste: [2, 3, 6, 7, 10, 1];.
+// Valor esperado no retorno da função: 4.
+
+function maiorValor (array) {
+    let maior = 0;
+    
+    for (let index = 0; index < array.length; index += 1) {
+        if (array[index] > maior) {
+            maior = index;
+        }
+    }
+    return maior
+}
+
+let arrayNumeros = [2, 3, 6, 7, 10, 1];
+console.log(maiorValor(arrayNumeros));
+
+// 3- Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// Array de teste: [2, 4, 6, 7, 10, 0, -3];.
+// Valor esperado no retorno da função: 6.
+
+function menorValor (array) {
+    let menor = 0;
+    
+    for (let index = 0; index < array.length; index += 1) {
+        if (array[index] < menor) {
+            maior = index;
+        }
+    }
+    return maior
+}
+
+let arrayNumeros2 = [2, 4, 6, 7, 10, 0, -3];
+console.log(menorValor(arrayNumeros2));
+
+// 4- Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+// Valor esperado no retorno da função: Fernanda.
+
+function nomeMaior (array) {
+    let maior = 0;
+    
+    for (let index = 0; index < array.length; index += 1) {
+        if (array[index].length > maior && array[index].length > array[index + 1].length) {
+            maior = array[index];
+        }    
+    }
+    return maior
+}
+
+let arrayNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log(nomeMaior(arrayNomes));
+
+// 5- Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+// Valor esperado no retorno da função: 2.
+// Incompleto
+function testeRepeticao(array) {
+    let cont = 0;
+    for (let index = 0; index < array.length; index += 1) {
+        for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
+            if (array[index] == array[secondIndex]) {
+                cont += 1
+                break;
+            }
+        }
+    }
+    return cont
+}
+
+let arrayTeste = [2, 3, 2, 5, 8, 2, 3];
+console.log(testeRepeticao(arrayTeste))
+
+// 6- Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+// Valor de teste: N = 5.
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15.
+
+// 7- Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+// Valor de teste: 'trybe' e 'be'
+// Valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be');
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan');
+// Retorno esperado: false
