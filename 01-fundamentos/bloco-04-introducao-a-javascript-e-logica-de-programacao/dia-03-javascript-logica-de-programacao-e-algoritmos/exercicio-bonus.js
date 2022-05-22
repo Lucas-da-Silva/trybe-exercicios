50bea27e99ada2876689430d0eee0889f83f360e
lucas-da-silva <lucassouza00012345@gmail.com>
2022-05-22 10:36:36 -0300

    Tentando terminar o exercicio-bonus

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus.js
@@ -1,4 +1,4 @@
-// Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+// 1- Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
 let n = 4;
 let aux = '';
@@ -11,55 +11,49 @@ for (let linha = 1; linha <= n; linha += 1) {
     aux = '';
 }
 
-// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
 
 let numero = 5;
 let asteriscos = '';
 
 for (let linha = 1; linha <= numero; linha += 1) {
-    // console.log(l)
+    
     for (let coluna = 1; coluna <= numero; coluna += 1){
-    if (linha == coluna) {
+        if (linha == coluna) {
         asteriscos += "*"
-    }
+        }
     }
     console.log(asteriscos)
 }
 
-// Agora inverta o lado do triângulo.
-// Incompleto
+// 3- Agora inverta o lado do triângulo.
 
-let n1 = 5;
-let a = '';
-let espaco = 0;
-let espacoEsquerda = '';
+function invertido(n) {
+    for (let index = 1; index <= n; index += 1) {
+      let result = "";
 
-for (let linha = 1; linha <= n1; linha += 1) {
+      for (let espacosContador = 1; espacosContador <= n - index; espacosContador += 1) {
+        result += " "
+      }
 
-    for (let coluna = 1; coluna <= n1; coluna += 1){
-        if (linha == coluna) {
-            a += "*"
+      for (let asteristicosContador = 1; asteristicosContador <= index; asteristicosContador += 1) {
+        result += "*"
+      }
+
+      console.log(result)
     }
-        if (a.length < n1) {
-            espaco = n1 - a.length
-            console.log(espaco)
-            //for (let possiveisEspacos = 0; possiveisEspacos < espaco; possiveisEspacos += 1)
-            //espacoEsquerda += ' ';
-        }
-    }
-    console.log(espacoEsquerda + a)
-}
+  }
+
+invertido(5);
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
 
 
-// Depois, faça uma pirâmide com n asteriscos de base:
-//Incompleto
+// 6- Faça um programa que diz se um número definido numa variável é primo ou não.
 
-// Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
-//Incompleto
-
-// Faça um programa que diz se um número definido numa variável é primo ou não.
-
-let testePrimo = 4;
+let testePrimo = 3;
 let ehPrimo = true;
 
 for (let numeros = 2; numeros < testePrimo; numeros += 1) {
