eda7c955e5271b61a64a70cebfe9deb1d0bde2cf
lucas-da-silva <lucassouza00012345@gmail.com>
2022-05-22 12:12:49 -0300

    Adiciona o resto do conteúdo do bloco 01

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-parte-02-funcoes.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-parte-02-funcoes.js
@@ -68,22 +68,6 @@ console.log(nomeMaior(arrayNomes));
 // 5- Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 // Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 // Valor esperado no retorno da função: 2.
-// Incompleto
-function testeRepeticao(array) {
-    let cont = 0;
-    for (let index = 0; index < array.length; index += 1) {
-        for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
-            if (array[index] == array[secondIndex]) {
-                cont += 1
-                break;
-            }
-        }
-    }
-    return cont
-}
-
-let arrayTeste = [2, 3, 2, 5, 8, 2, 3];
-console.log(testeRepeticao(arrayTeste))
 
 // 6- Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
 // Valor de teste: N = 5.
