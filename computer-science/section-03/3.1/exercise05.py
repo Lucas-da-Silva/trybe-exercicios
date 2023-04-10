70727a9f1faf7b5b3f4ad8e083bf91ffdf7733fd
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-10 20:49:12 -0300

    feat: adds exercises 01,02,03 and 04 complete of day 3.1 computer-science

---
+++ computer-science/section-03/3.1/exercise05.py
@@ -0,0 +1,19 @@
+import requests
+from parsel import Selector
+
+URL_BASE = "http://books.toscrape.com/catalogue/"
+
+response = requests.get(URL_BASE + "the-grand-design_405/index.html")
+selector = Selector(response.text)
+
+title = selector.css(".product_main h1::text").get()
+price = selector.css(".price_color::text").re_first(r"£([0-9]+\.[0-9]+)")
+description = selector.css("#product_description + p::text").get()
+cover = URL_BASE + selector.css("#product_gallery img::attr(src)").get()
+availability = selector.css(".product_main .availability::text").re_first("\d")
+
+suffix_description = "...more"
+if description.endswith(suffix_description):
+    description = description.removesuffix(suffix_description)
+
+print(title, price, description, cover, availability, sep=", ")
